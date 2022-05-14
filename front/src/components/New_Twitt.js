5d6f78ff581c4c2f55a99f306dd2f266e7655476
ErnestoDamian <ernesto.roca@sansano.usm.cl>
2022-05-13 21:45:26 -0400

    implementacion del componente New_Twitt, falta el formulario para ingresar los datos por la pagina

--- front/src/components/New_Twitt.js
+++ front/src/components/New_Twitt.js
@@ -1,13 +1,35 @@
-import React, { Component } from "react";
+import React,{useEffect, useState} from "react";
+import axios from 'axios'
 
-class New_Twitt extends Component {
-  render() {
-    return (
-      <div>
-        <h3 className="center">Escribe un nuevo tweet </h3>
-      </div>
-    );
-  }
+function New_Twitt(){
+  
+  const [loaded, setLoaded] = useState(false);
+
+  let autor, texto;
+
+  useEffect(()=>{
+    const fetchData = async() =>{
+      if(!loaded){
+        try {
+          const result = await axios.post('http://localhost:3002/new-tweet',{
+            author: autor,
+            text: texto
+          })
+          console.log(result)
+          setLoaded(true)
+        } catch (error) {
+          console.log(error)
+        }
+      }
+    }
+    fetchData()
+  })
+  return (
+    <div>
+      <h3 className="center">Escribe un nuevo tweet</h3>
+    </div>
+  );
+
 }
 
 export default New_Twitt;
